8d8a83b72cc63b33b3c02b4dd0d999c0777cc54d
AryanKalwani <50944036+AryanKalwani@users.noreply.github.com>
2020-08-18 00:15:15 +0530

    Adding the Alert component

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -7,6 +7,7 @@ export default function (state = initialState, action) {
 
   switch (type) {
     case 'SET_ALERT':
+      console.log('called');
       return [...state, payload];
     case 'REMOVE_ALERT':
       return state.filter((alert) => alert.id !== payload);

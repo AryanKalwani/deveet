5c324c9c962978050ab59c827403094eabfb9634
AryanKalwani <50944036+AryanKalwani@users.noreply.github.com>
2020-08-20 22:33:55 +0530

    adding protected route for the dashboard route

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 import './App.css';
 import { Provider } from 'react-redux';
 import store from './store';
@@ -28,6 +30,7 @@ const App = () => {
           <Switch>
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
+            <PrivateRoute exact path="/dashboard" component={Dashboard} />
           </Switch>
         </Fragment>
       </Router>
